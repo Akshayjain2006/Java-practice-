a5e9629a1b3e524acc20e9444843d28ed69a049c
Akshay <mrakshayjain01@gmail.com>
2024-07-14 10:59:06 +0530

    core java

---
+++ A.java
@@ -0,0 +1,9 @@
+public class A {
+    public static void main(String [] args) {
+        System.out.println("Burger" );
+        System.out.println("Chole bhature" );
+        System.out.println("Dosa" );
+        System.out.println("Rajma chawal" );
+        System.out.println("Dal bati" );
+    }
+}
